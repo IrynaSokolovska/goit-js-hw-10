29960141f9d2345d2f9196ebb505c97a2686fce5
IrynaSokolovska <122970815+IrynaSokolovska@users.noreply.github.com>
2023-07-01 06:39:09 +0000

    Deploying to gh-pages from @ IrynaSokolovska/goit-js-hw-10@e6ad53ecedf06ebb9c38a2ef7e41b5c8e32ea3df 🚀

--- index.7a6a056c.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users");
-//# sourceMappingURL=index.7a6a056c.js.map
--- index.cf27f9d5.js
+++
@@ -1,2 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users");
-//# sourceMappingURL=index.cf27f9d5.js.map
